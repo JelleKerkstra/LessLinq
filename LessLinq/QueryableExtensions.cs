9bea9c498b84165f0bb89f041b43c1429c390a3e
Jelle Kerkstra <j.kerkstra@viscon.eu>
2019-11-04 20:43:14 +0100

    Add QueryableExtensions.With

--- LessLinq/QueryableExtensions.cs
+++ LessLinq/QueryableExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using NeinLinq;
@@ -20,6 +21,33 @@ namespace LessLinq
             Expression<Func<T, TPredicateSource>> path, Expression<Func<TPredicateSource, bool>> predicate)
         {
             return set.Where(predicate.Translate().To(path));
+        }
+
+        /// <summary>
+        /// Filters the queryable by the given values on the selected property
+        /// </summary>
+        /// <typeparam name="T">Type of sequence.</typeparam>
+        /// <typeparam name="TProperty">Type of the filtered property.</typeparam>
+        /// <param name="set">The queryable on which the filter is to be applied.</param>
+        /// <param name="propertySelector">Expression to select the property on the sequence type.</param>
+        /// <param name="values">The values to filter the sequence by.</param>
+        /// <returns>A sequence filtered by the given values on the given property.</returns>
+        public static IQueryable<T> With<T, TProperty>(this IQueryable<T> set,
+            Expression<Func<T, TProperty>> propertySelector, IEnumerable<TProperty> values)
+        {
+            if (!values.Any()) return set;
+            if (!(propertySelector.Body is MemberExpression memberExpr))
+                throw new ArgumentException("The propertySelector should be a MemberExpression!", nameof(propertySelector));
+
+            var param = Expression.Parameter(typeof(T));
+            var prop = Expression.Property(param, memberExpr.Member.Name);
+
+            var body = values
+                .Select(t => Expression.Equal(prop, Expression.Constant(t)))
+                .Aggregate(Expression.Or);
+            var predicate = Expression.Lambda<Func<T, bool>>(body, param);
+
+            return set.Where(predicate);
         }
     }
 }
